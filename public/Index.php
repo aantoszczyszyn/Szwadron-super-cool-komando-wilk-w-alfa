345ccdc02457afc7835a71c42e0c6e9c1eeec0d0
Xomil <xomilol@gmail.com>
2025-01-10 22:44:30 +0100

    Restrukturyzacja 2.0, grupowa praca nad html i css oraz php

---
+++ public/Index.php
@@ -0,0 +1,40 @@
+<?php
+require_once __DIR__ . '/../src/Model/Router.php';
+require_once __DIR__ . '/../src/Controller/Controller.php';
+require_once __DIR__ . '/../src/Apis/ServiceApi.php';
+
+use App\Model\Router;
+use App\Controller\Controller;
+
+// Front-Controller
+
+$router = new Router();
+$router->addRoute('/', Controller::class, 'showIndex');
+
+?>
+
+
+<!doctype html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Nowy lepszy plan ZUT 2025</title>
+        <link rel="stylesheet" href="styles/style.css">
+        <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
+    </head>
+    <body>
+        <?php
+            try {
+                // Przekierowanie żądania na odpowiednią akcję kontrolera
+                $router->dispatch($_SERVER['REQUEST_URI']);
+            } catch (Exception $e) {
+                die('Wystąpił błąd: ' . $e->getMessage());
+            }
+        ?>
+        <footer>
+            ZUT 2025
+        </footer>
+        <script type="module" src="/scripts/Script.js"></script>
+    </body>
+</html>
---
+++ src/Controller/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+class Controller{
+
+    private ServiceApi $serviceApi;
+
+    public function __construct(){
+//         $this->serviceApi = new ServiceApi;
+    }
+
+    public function showIndex(){
+        include __DIR__ . '/../Views/Header/Header.php';
+        include __DIR__ . '/../Views/Schedule/Inputs.php';
+
+    }
+
+}
+
+?>
