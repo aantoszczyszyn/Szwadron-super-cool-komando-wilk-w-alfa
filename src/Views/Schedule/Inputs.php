b5997defdfdcca7242b94592a833e9a3fb8b67bf
Xomil <xomilol@gmail.com>
2025-01-15 21:06:25 +0100

    Podwaliny funkcjonujacego formularza. Milego wieczoru :)

--- src/Views/Schedule/Inputs.php
+++ src/Views/Schedule/Inputs.php
@@ -2,42 +2,44 @@
 
 ?>
 
-<form id="searchForm">
+<form id="searchForm" method="POST">
     <div class="form-group">
         <label for="studentNumber">Numer albumu studenta</label><br>
-        <input type="text" class="form-control" id="studentNumber" placeholder="Podaj numer albumu">
+        <input type="text" class="form-control" id="student_id" name="student_id" placeholder="Podaj numer albumu">
     </div>
 
     <div class="form-group">
         <label for="teacherName">Nazwisko prowadzącego</label><br>
-        <input type="text" class="form-control" id="teacherName" placeholder="Podaj nazwisko">
+        <input type="text" class="form-control" id="worker_name" name="worker_name" placeholder="Podaj nazwisko">
     </div>
 
     <div class="form-group">
         <label for="subject">Przedmiot</label><br>
-        <input type="text" class="form-control" id="subject" placeholder="Podaj przedmiot">
+        <input type="text" class="form-control" id="subject_name" name="subject_name" placeholder="Podaj przedmiot">
     </div>
 
     <div class="form-group">
         <label for="group">Grupa</label><br>
-        <input type="text" class="form-control" id="group" placeholder="Podaj grupę">
+        <input type="text" class="form-control" id="group_name" name="group_name" placeholder="Podaj grupę">
     </div>
 
     <div class="form-group">
         <label for="room">Sala</label><br>
-        <input type="text" class="form-control" id="room" placeholder="Podaj salę">
+        <input type="text" class="form-control" id="room" name="room" placeholder="Podaj salę">
     </div>
 
-    <div class="form-group">
+<!--     <div class="form-group">
         <label for="studyType">Typ studiów</label><br>
         <select class="form-control" id="studyType">
             <option value="">Wybierz typ studiów</option>
             <option value="stacjonarne">Stacjonarne</option>
             <option value="niestacjonarne">Niestacjonarne</option>
         </select>
-    </div>
+    </div> -->
+
+
     <div class="form-group">
-        <button type="submit" id="btn_search">Szukaj</button>
+        <button id="btn-search">Prześlij</input>
         <button type="button" id="btn_print">Drukuj</button>
         <button type="button" id="btn_help">Pomoc</button>
     </div>
